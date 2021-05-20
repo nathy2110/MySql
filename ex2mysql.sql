0620b41f0d0a5b6ddf4496d73c6eb57c5517a05c
Nathalia Beatriz Silva Pereira <83587907+nathy2110@users.noreply.github.com>
2021-05-20 18:47:33 -0300

    Add files via upload

---
+++ ex2mysql.sql
@@ -0,0 +1,14 @@
+use db_ex2;
+select * from tb_eco where preco<500;
+
+select * from tb_eco where preco>500;
+insert into tb_eco (calcas,tenis,eletrodomestiscos,móveis,preco) values ("médios","medio","lg","cobre",700);
+select * from tb_eco where preco=700;
+select * from tb_eco;
+insert into tb_eco (calcas,tenis,eletrodomestiscos,móveis,preco) values ("médios","medio","lg","cobre",800);
+select * from tb_eco;
+
+
+
+
+
